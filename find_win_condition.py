4c930d157f5a006090922d60260ede9e13093bba
Garner Thompson <garnerthompson@Garners-MacBook-Pro-2.local>
2024-05-18 16:15:29 -0400

    dummy find condition

--- find_win_condition.py
+++ find_win_condition.py
@@ -1,3 +1,5 @@
-
+import numpy as np
 def find_win_condition(win_table) :
-    return
+    condition = np.where(win_table == 1, True, False)
+    condition = np.where(condition)[0]
+    return condition
