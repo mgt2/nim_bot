09f0558908e2f3ba49a61268ccff2beb9195519d
Garner Thompson <garnerthompson@Garners-MacBook-Pro-2.local>
2024-05-19 19:51:58 -0400

    bot vs bot and data collection code

---
+++ bot_play.py
@@ -0,0 +1,38 @@
+import random
+from generate_win_table import generate_win_table
+
+def make_move(n, moves, win_table, prob_correct_move) :
+    if random.random() < prob_correct_move :
+        if win_table[n] == 1 :
+            for poss_move in moves :
+                if n - poss_move >= 0 and win_table[n - poss_move] == 2 :
+                    move = poss_move
+                    return move
+        else :
+            valid_moves = []
+            for poss_move in moves :
+                if n - poss_move >= 0 :
+                    valid_moves.append(poss_move)
+            move = random.choice(valid_moves)
+            return move
+    else :
+        valid_moves = []
+        for poss_move in moves :
+            if n - poss_move >= 0 :
+                valid_moves.append(poss_move)
+        move = random.choice(valid_moves)
+    return move
+
+def play_bot_game(bot1_acc, bot2_acc, moves, n) :
+    win_table = generate_win_table(moves, n)
+    player1_turn = True
+    while n > 0 :
+        if player1_turn :
+            move = make_move(n, moves, win_table, bot1_acc)
+        else :
+            move = make_move(n, moves, win_table, bot2_acc)
+        n -= move
+        if n == 0 :
+            return player1_turn
+        player1_turn = not player1_turn
+    return RuntimeError
---
+++ collect_bot_data.py
@@ -0,0 +1,51 @@
+import numpy as np
+from generate_win_table import generate_win_table
+from bot_play import play_bot_game
+
+def run_trials(n, moves, bot1_acc, bot2_acc, num_trials=100) :
+    return [play_bot_game(bot1_acc, bot2_acc, moves, n) for _ in range(num_trials)]
+
+def analyze_trials(results) :
+    player1_wins = np.sum(results)
+    player2_wins = len(results) - player1_wins
+    return player1_wins/(player1_wins + player2_wins)
+
+def collect_bot_data_one_game(n, moves, bot_accs, compiled_array, win_table, num_trials_each=100) :
+    for i in range(len(bot_accs)) :
+        (bot1_acc, bot2_acc) = bot_accs[i]
+        results = run_trials(n, moves, bot1_acc, bot2_acc, num_trials_each)
+        player1_winrate = analyze_trials(results)
+        compiled_array[n][i] = player1_winrate
+    return compiled_array
+
+def collect_bot_data_same_moves(max_n, moves, bot_accs, num_trials_each=100) :
+    win_table = generate_win_table(moves, max_n)
+    ground_truth = np.where(win_table == 1, 1, 0)
+
+    compiled_array = np.zeros(max_n + 1, len(bot_accs))
+    for n in range(max_n + 1) :
+        compiled_array = collect_bot_data_one_game(n, moves, bot_accs, compiled_array, win_table, num_trials_each)
+    return compiled_array, ground_truth
+
+def collect_bot_data(max_n, moves_list, bot_accs, num_trials_each=100) :
+    compiled_arrays = []
+    ground_truths = []
+
+    for moves_set in moves_list :
+        compiled_array, ground_truth = collect_bot_data_same_moves(max_n, moves_set, bot_accs, num_trials_each)
+        compiled_arrays.append(compiled_array)
+        ground_truths.append(ground_truth)
+
+    return compiled_arrays, ground_truths
+
+def collect_bot_data_grid(max_n, moves_list, min_bot_acc, max_bot_acc, acc_increment, num_trials_each=100) :
+    bot_accs = [(acc1, acc2) for acc1 in np.arange(min_bot_acc, max_bot_acc, acc_increment) for acc2 in np.arange(min_bot_acc, max_bot_acc, acc_increment)]
+    compiled_arrays, ground_truths = collect_bot_data(max_n, moves_list, bot_accs, num_trials_each)
+
+    grid_array = np.zeros((len(moves_list), max_n + 1, len(np.arange(min_bot_acc, max_bot_acc, acc_increment)), len(np.arange(min_bot_acc, max_bot_acc, acc_increment))))
+    for i in range(len(compiled_arrays)) :
+        for j in range(max_n + 1) :
+            for k in range(len(bot_accs)) :
+                (bot1_acc, bot2_acc) = bot_accs[k]
+                grid_array[i][j][bot1_acc][bot2_acc] = compiled_arrays[i][j][k]
+    return grid_array, ground_truths
