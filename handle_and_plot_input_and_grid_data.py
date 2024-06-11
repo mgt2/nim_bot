1a7d85bcb5971cce1772bdd2ee9c62a34a414590
Garner Thompson <garnerthompson@Garners-MacBook-Pro-2.local>
2024-06-11 00:24:24 -0400

    plotting trend framework

--- handle_and_plot_input_and_grid_data.py
+++ handle_and_plot_input_and_grid_data.py
@@ -5,7 +5,7 @@ from generate_win_table import generate_win_table
 from collect_bot_data import collect_bot_data_grid
 from distance import calc_win_distance
 
-def plot_distances(distances, moves_list) :
+def plot_distances_sequentially(distances, moves_list) :
     for i in range(len(distances)) :
         for j in range(len(distances[i])) :
             plt.imshow(distances[i][j], cmap='hot', interpolation='nearest')
@@ -21,6 +21,30 @@ def plot_distances(distances, moves_list) :
 
             plt.show()
     return
+
+def plot_distances(distances, moves_list):
+    num_rows = len(distances)
+    num_cols = len(distances[0])
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            axes[i, j].imshow(distances[i][j], cmap='hot', interpolation='nearest')
+            if j == 0:
+                axes[i, j].set_title(str(moves_list[i]) + ", 0 sticks")
+            else:
+                axes[i, j].set_title(str(j) + " sticks")
+            # axes[i, j].set_xlabel("Bot 2 Accuracy (0-10)")
+            # axes[i, j].set_ylabel("Bot 1 Accuracy (0-10)")
+            # for k in range(len(distances[i][j])):
+            #     for l in range(len(distances[i][j][k])):
+            #         axes[i, j].text(l, k, f"{distances[i][j][k][l]:.2f}", ha='center', va='center', color='green')
+
+            # Add a colorbar to show the mapping of values to colors
+    #fig.colorbar(axes[i, j].imshow(distances[i][j], cmap='hot', interpolation='nearest'), ax=axes[i, j])
+
+    plt.show()
 
 def load_data(data_file_path) :
     with open(data_file_path, 'r') as f :
---
+++ plot_distance_trends.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from handle_and_plot_input_and_grid_data import load_data, write_to_file, plot_distances, plot_distances_sequentially
+from generate_win_table import generate_win_table
+
+# Assumes distance data is configured like in output_data folder
+def configure_distance_trends(distance_data) :
+    new_distance_data = np.zeros((len(distance_data['moves']), len(distance_data['0'][0]), len(distance_data['0'][0][0]), len(distance_data['0'])))
+
+    for i in range(len(distance_data['moves'])) :
+        for j in range(len(distance_data[str(i)])) :
+            for k in range(len(distance_data[str(i)][j])) :
+                for l in range(len(distance_data[str(i)][j][k])) :
+                    new_distance_data[i][k][l][j] = distance_data[str(i)][j][k][l]
+    return new_distance_data
+
+def plot_distance_trends(distance_data, plot_info, moves_list) :
+    for ind in plot_info['move_indices'] :
+        win_table = generate_win_table(moves_list[ind], plot_info['max_n'])
+        for j in range(int(10 * plot_info['min_acc_p1']), int(10 * plot_info['max_acc_p1']) + 1, int(10 * plot_info['acc_increment_p1'])) :
+            for k in range(int(10 * plot_info['min_acc_p2']), int(10 * plot_info['max_acc_p2']) + 1, int(10 * plot_info['acc_increment_p2'])) :
+                plot_list = []
+                x_values = []
+                for l in range(plot_info['min_n'], plot_info['max_n'] + 1, plot_info['n_increment']) :
+                    if win_table[l] in plot_info['player_wins'] :
+                        plot_list.append(distance_data[ind][j][k][l])
+                        x_values.append(l)
+
+                plt.plot(x_values, plot_list, label=f"Bot 1: {j/10}, Bot 2: {k/10}")
+        plt.xlabel("Number of Sticks")
+        plt.ylabel("Average Distance from Perfect Play Outcome")
+        plt.title(f"Distance Trends for {moves_list[ind]} with winners {plot_info['player_wins']}")
+        plt.legend()
+        plt.show()
+    return
+
+if __name__ == "__main__" :
+    dist_data = load_data("output_data/distances_x_x+1.json")
+
+    new_dist_data = configure_distance_trends(dist_data)
+
+    plot_info = {
+        'min_acc_p1' : 0.1,
+        'max_acc_p1' : 1,
+        'acc_increment_p1' : 0.3,
+        'min_acc_p2' : 0.8,
+        'max_acc_p2' : 0.8,
+        'acc_increment_p2' : 0.1,
+
+        'min_n' : 0,
+        'max_n' : 100,
+        'n_increment' : 1,
+
+        'move_indices' : [0, 1, 20],
+        'player_wins' : [1],
+    }
+
+    plot_distance_trends(new_dist_data, plot_info, dist_data['moves'])
